07fd0b207ccc41ea91718b33bf5c335e09de7586
NG ROCKERS <77508122+NGROCKERS@users.noreply.github.com>
2023-01-02 21:51:04 +0400

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-   return 'GreyMatters_Bot'
+   return 'ADARSHMEDIAS'
 
 #Ex https://Greymattersbot:ghp_147bkkabcdefgh@github.com/Greymattersbot/Mogenius
-os.system("git clone https://GITHUB-USERNAME:token@github.com/GITHUB-USERNAME/YOUR-REPO-NAME okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 main.py &")
+os.system("git clone https://NGROCKERS:@github.com/NGROCKERS/FileToLink okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 -m FileToLink &")
