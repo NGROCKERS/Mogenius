8ac68f5e8a1f83bf9d9cee45da3666edc66d7732
NG ROCKERS <77508122+NGROCKERS@users.noreply.github.com>
2023-01-24 21:00:08 +0400

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
    return 'ADARSHMEDIAS'
 
 #Ex https://Greymattersbot:ghp_147bkkabcdefgh@github.com/Greymattersbot/Mogenius
-os.system("git clone https://NGROCKERS:@https://github.com/NGROCKERS/Filestreambot-pro okk && cd okk && pip3 install -U -r requirements.txt && nohup python -m Adarsh &")
+os.system("git clone https://NGROCKERS:@https://github.com/NGROCKERS/Auto-Filter-V5 okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 bot.py &")
